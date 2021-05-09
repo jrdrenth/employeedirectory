afff561ac2c00e47de2b0f945280d388344722e7
jrdrenth <john.drenth@gmail.com>
2021-05-08 22:47:05 -0700

    Updates

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Export an object containing methods we'll use for accessing the RandomUser API
-
-export default {
-  getUsers: function() {
-    return axios.get("https://randomuser.me/api/?results=100&nat=us");
-  }
-};
